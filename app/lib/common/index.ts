ca32b8523898299e10edc6ee0c8f2938e7c952e6
Jesse Sivonen <j.sivonen@hotmail.com>
2025-05-14 15:43:27 +0300

    Remove collection store and its loader

--- app/lib/common/index.ts
+++ app/lib/common/index.ts
@@ -1,3 +1,2 @@
-export * from './json'
 export * from './models'
 export * from './constants'
--- app/lib/loaders/index.ts
+++ app/lib/loaders/index.ts
@@ -1,9 +1,9 @@
 import { SiteGroup } from '$lib/common/models/SiteGroup'
 import { Libraries, SiteGroups, Sites } from '$lib/pocketbase/collections'
 import { createStoreLoader } from '$lib/loaders/StoreLoader'
-import { createCollectionStoreLoader } from './CollectionStoreLoader'
 import type { Site } from '$lib/common/models/Site'
 import type { Id } from '$lib/common/models/Id'
+import { readable } from 'svelte/store'
 
 export const require_marketplace_symbol_groups = createStoreLoader(() => fetch('https://weave-marketplace.vercel.app/api/symbol_groups').then((response) => response.json()), [])
 export const require_marketplace_symbols = createStoreLoader((group_id: number) => fetch(`https://weave-marketplace.vercel.app/api/symbol_groups/${group_id}`).then((response) => response.json()), [])
@@ -13,5 +13,5 @@ export const require_site_list = createStoreLoader<Pick<Site, 'id' | 'name' | 'd
 	(site_group_id) => Sites.getFullList({ filter: site_group_id ? `group.id = "${site_group_id}"` : undefined, fields: 'id, name, description, group, index' }),
 	[]
 )
-export const require_library = createCollectionStoreLoader(Libraries, () => Libraries.getList(0, 1).then(([record]) => record))
-export const require_site = createCollectionStoreLoader(Sites, async (site_id: string) => site_id)
+export const require_library = () => readable(null)
+export const require_site = () => readable(null)
