d9d48e4ed1832a188f70f946ac9a20ad0d3a90ca
Jesse Sivonen <j.sivonen@hotmail.com>
2025-07-24 14:56:58 +0300

    Set marketplace URL

--- app/lib/pocketbase/PocketBase.ts
+++ app/lib/pocketbase/PocketBase.ts
@@ -5,7 +5,7 @@ export const self = new PocketBase(import.meta.env.DEV ? 'http://127.0.0.1:8090'
 // Disable auto-cancellation for debugging
 self.autoCancellation(false)
 
-export const marketplace = new PocketBase('about:blank') // TODO
+export const marketplace = new PocketBase('https://marketplace.palacms.org')
 
 export const user = () => {
 	return self.authStore.record as unknown as User | undefined
