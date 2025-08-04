479633f2718f2ed2d2975d4db2487d4a78a76121
Jesse Sivonen <j.sivonen@hotmail.com>
2025-08-04 13:55:26 +0300

    Re-enable auto-cancellation for PocketBase client

--- app/lib/pocketbase/PocketBase.ts
+++ app/lib/pocketbase/PocketBase.ts
@@ -2,9 +2,6 @@ import type { User } from '$lib/common/models/User'
 import PocketBase from 'pocketbase'
 
 export const self = new PocketBase(import.meta.env.DEV ? 'http://127.0.0.1:8090' : '/')
-// Disable auto-cancellation for debugging
-self.autoCancellation(false)
-
 export const marketplace = new PocketBase('https://marketplace.palacms.org')
 
 export const user = () => {
